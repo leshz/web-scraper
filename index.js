6d0c9415db9b8c1b24e8ea878dabae9b63d1b6a3
Jeff Barragan <leshz@me.com>
2023-04-10 18:21:01 -0500

    fix path function useless

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const spinner = ora('Loading scrapper').start()
 const readAndLoadFile = async ({ fileName = 'accounts.csv', encoding = 'utf8', delimiter = '' }) => {
   const styleDeLimiter = delimiter === 'google' ? ',' : ';'
   try {
-    const accountsFile = await fs.readFileSync(path.join(path.resolve(fileName)), { encoding })
+    const accountsFile = await fs.readFileSync(path.resolve(fileName), { encoding })
     const accounts = csv.toSchemaObject(accountsFile, { delimiter: styleDeLimiter })
     const isValidUser = accounts.every(item => item?.user)
     const isValidPass = accounts.every(item => item?.pass)
